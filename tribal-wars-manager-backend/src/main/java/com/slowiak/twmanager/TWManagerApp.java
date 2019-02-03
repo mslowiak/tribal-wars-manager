591c41b66883a961989b804a54b95d73ad87a6fd
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-02-03 14:34:34 +0100

    Init repository

---
+++ tribal-wars-manager-backend/src/main/java/com/slowiak/twmanager/TWManagerApp.java
@@ -0,0 +1,14 @@
+package com.slowiak.twmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TWManagerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TWManagerApp.class, args);
+	}
+
+}
+
---
+++ tribal-wars-manager-backend/src/test/java/com/slowiak/twmanager/TWManagerAppTests.java
@@ -0,0 +1,17 @@
+package com.slowiak.twmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TWManagerAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
