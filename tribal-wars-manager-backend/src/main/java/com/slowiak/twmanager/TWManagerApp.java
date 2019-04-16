45112a88b193cdedde6dd4ee1efd115206c9d05a
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-04-16 12:34:32 +0200

    Procfile example

--- tribal-wars-manager-backend/src/main/java/com/slowiak/twmanager/TWManagerApp.java
+++ tribal-wars-manager-backend/src/main/java/com/slowiak/twmanager/TWManagerApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TWManagerApp {
 	public static void main(String[] args) {
+		System.out.println(System.getProperty("name"));
 		SpringApplication.run(TWManagerApp.class, args);
 	}
 }
